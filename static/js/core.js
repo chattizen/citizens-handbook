6972ebb6ae434049e52962a9767a1c3accc2883d
Daniel Ryan <dryan@dryan.com>
2014-08-09 13:29:03 -0400

    prep work for showing district boundaries

--- static/js/core.js
+++ static/js/core.js
@@ -2,6 +2,42 @@
 window.chattizen    =   window.chattizen || {};
 var chattizen       =   window.chattizen;
 
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisArg */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+      {
+        var val = t[i];
+
+        // NOTE: Technically this should Object.defineProperty at
+        //       the next index, as push can be affected by
+        //       properties on Object.prototype and Array.prototype.
+        //       But that method's new, and collisions should be
+        //       rare, so use the more-compatible alternative.
+        if (fun.call(thisArg, val, i, t))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
+}
+
 (function($, Modernizr) {
     "use strict";
 
@@ -9,7 +45,9 @@ var chattizen       =   window.chattizen;
     html            =   $('html'),
     showNav         =   $('#show-nav'),
     maps            =   $('.map'),
-    corejs          =   $('#corejs');
+    corejs          =   $('#corejs'),
+    division        =   $('#division'),
+    noop            =   function() {};
 
     chattizen.apiKeys   =   {
         'google':       corejs.data('google-api-key'),
@@ -80,5 +118,11 @@ var chattizen       =   window.chattizen;
             }
         }
     });
+
+    function ocdAPICall(ocd_id, callback, errCallback) {
+        $.getJSON(
+            ['https://api.opencivicdata.org/ocd-division', ocd_id, '?apikey=', chattizen.apiKeys.sunlight, '&callback=?'].join('')
+        ).done(callback || noop).fail(errCallback || noop);
+    }
 
 })(window.jQuery, window.Modernizr);
